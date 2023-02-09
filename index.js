a8c4ac77d5452887c784a6902abdea36c8f3f7b2
divine <divinemaina@gmail.com>
2023-02-09 13:03:01 +0200

    fix linters

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const burger =document.getElementById('fa-bars')
-const hideMenu =document.getElementById('closeIcon')
-const navLink =document.querySelector('.nav-link')
-const navLinks =document.getElementById('desktop-nav')
+const burger = document.getElementById('fa-bars');
+const hideMenu = document.getElementById('closeIcon');
+const navLink = document.querySelector('.nav-link');
+const navLinks = document.getElementById('desktop-nav');
 
 burger.onclick = function burger() {
-    navLinks.style.right = '0';
+  navLinks.style.right = '0';
+};
+hideMenu.onclick = function hideMenu() {
+  navLinks.style.right = '-100%';
+};
 
-  };
-  hideMenu.onclick = function hideMenu() {
-    navLinks.style.right = '-100%';
-  };
-  
- navLink.onclick = function hideMenu() {
-    navLinks.style.right = '-100%';
-  };
-  
+navLink.onclick = function hideMenu() {
+  navLinks.style.right = '-100%';
+};
